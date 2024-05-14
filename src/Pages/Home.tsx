8379b42ae04766a8bf04b16bb9d0c5b1f17a0ae7
fakhraldeen18 <fakhraldeen18@gmail.com>
2024-05-14 14:19:53 +0300

    some updates

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,50 +1,69 @@
-import api from "@/api";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Product } from "@/types";
-import { useQuery } from "@tanstack/react-query";
+import api from "@/api"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+import { Product } from "@/types"
+import { CardBody, Typography } from "@material-tailwind/react"
+import { useQuery } from "@tanstack/react-query"
 
-export function Home  (){
-    const getProducts = async () => {
-      try {
-        const res = await api.get("/product")
-        return res.data
-      } catch (error) {
-        console.error(error)
-        return Promise.reject(new Error("Something went wrong"))
-      }
+export function Home() {
+  const getProducts = async () => {
+    try {
+      const res = await api.get("/product")
+      return res.data
+    } catch (error) {
+      console.error(error)
+      return Promise.reject(new Error("Something went wrong"))
     }
+  }
 
-    // Queries
-    const { data, error } = useQuery<Product[]>({
-      queryKey: ["product"],
-      queryFn: getProducts
-    })
-    return (
-      <>
-        <h1 className="text-2xl uppercase mb-10">Products</h1>
-        <section className="flex flex-col md:flex-row gap-4 justify-between max-w-6xl mx-auto">
-          {data?.map((product) => (
-            <Card key={product.id} className="w-[250px]">
-              <CardHeader>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className=" md:w-40 md:h-full rounded-lg mx-auto"
-                />
-                <CardTitle>{product.name}</CardTitle>
-                <CardDescription>{product.price}</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content Here</p>
-              </CardContent>
-              <CardFooter>
-                <Button className="w-full">Add to cart</Button>
-              </CardFooter>
-            </Card>
-          ))}
-        </section>
-        {error && <p className="text-red-500">{error.message}</p>}
-      </>
-    )
+  // Queries
+  const { data, error } = useQuery<Product[]>({
+    queryKey: ["product"],
+    queryFn: getProducts
+  })
+  return (
+    <>
+      <h1 className="text-2xl uppercase mb-10">Products</h1>
+      <section className="flex flex-col md:flex-row gap-4 justify-between max-w-6xl mx-auto  flex-wrap">
+        {data?.map((product) => (
+          <Card key={product.id} className="w-[350px]">
+            <CardHeader className="h-80">
+              <img
+                src={product.image}
+                alt={product.name}
+                className=" h-full w-full object-cover rounded-lg"
+              />
+              <CardTitle>{product.name}</CardTitle>
+            </CardHeader>
+            <CardBody>
+              <div className="mb-2 flex items-center justify-between">
+                <Typography color="black" className="font-medium">
+                  {product.size}
+                </Typography>
+                <Typography className="font-medium">SR {product.price}</Typography>
+              </div>
+              {/* <Typography
+                variant="small"
+                style={{ color: "gray" }}
+                className="font-normal opacity-75"
+              >
+                {product.shortDesc}
+              </Typography> */}
+            </CardBody>
+            <CardFooter>
+              <Button className="w-full bg-[#701878]">Add to cart</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </section>
+      {error && <p className="text-red-500">{error.message}</p>}
+    </>
+  )
 }
--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react"
 import HeroImage from "../assets/Images/HeroBanner.png"
 
 export function HeroBanner() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 -mt-16">
-      <div className="container grid items-center gap-6 px-4 md:px-6 lg:grid-cols-[1fr_550px] lg:gap-12 xl:grid-cols-[1fr_650px]">
+    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-45 -mt-16">
+      <div className="container grid items-center gap-6 px-2 md:px-6 lg:grid-cols-[1fr_550px] lg:gap-12 xl:grid-cols-[1fr_650px]">
         <div className="space-y-4">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-            Immerse Yourself in the Future of VR
+          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl mt-20 xl:text-6xl/none">
+            Immerse Yourself in the Future of Virtual Reality
           </h1>
           <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
             Experience the latest in virtual reality technology with our cutting-edge VR headset.
             Explore new worlds, play immersive games, and unlock the full potential of VR.
           </p>
           <div className="flex flex-col gap-2 min-[400px]:flex-row">
-            <button></button>
             <a
               className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
               href="#"
@@ -22,7 +21,11 @@ export function HeroBanner() {
               Buy Now
             </a>
             <a
-              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200  bg-[#701878] px-8 text-sm font-medium shadow-sm 
+              transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none 
+              disabled:opacity-50
+            dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950
+            dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
               href="#"
             >
               Learn More
@@ -31,7 +34,7 @@ export function HeroBanner() {
         </div>
         <img
           alt="VR Headset"
-          className="mx-auto aspect-square overflow-hidden rounded-xl object-contain"
+          className="mx-auto aspect-square overflow-hidden rounded-xl object-contain hidden md:inline-block"
           height="550"
           src={HeroImage}
           width="550"
